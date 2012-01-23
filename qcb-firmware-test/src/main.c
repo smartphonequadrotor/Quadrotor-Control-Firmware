31004fe2331084718c78d1c224cf078c4dc8342f
Jordan Woehr <jordanwoehr@gmail.com>
2012-01-22 20:59:30 -0700

    Modified makefile to compile with C99. Basic debug code that turns stuff on on a button press.

--- qcb-firmware-test/src/main.c
+++ qcb-firmware-test/src/main.c
@@ -19,11 +19,37 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 *****************************************************************************/
 
+#include "AT91SAM7S161.h"
+
 int main(void)
 {
+	// Enable clock to Parallel IO Controll A (GPIOs)
+	AT91C_BASE_PMC->PMC_PCER = 0x00000004;
+
+	// Configure PWR_CNTL pin (PA31 as output), drive low to enable ESCs
+	AT91C_BASE_PIOA->PIO_PER = 0x80000000;
+	AT91C_BASE_PIOA->PIO_OER = 0x80000000;
+
+	// Configure BTN3 pin (PA30 as input), keep internal pull enabled,
+	// pulled low when pressed
+	AT91C_BASE_PIOA->PIO_PER = 0x40000000;
+
+	// Configure LED3 pin (PA19 as output), drive low to turn on
+	AT91C_BASE_PIOA->PIO_PER = 0x00080000;
+	AT91C_BASE_PIOA->PIO_OER = 0x00080000;
+
 	while(1)
 	{
-
+		if(AT91C_BASE_PIOA->PIO_PDSR & 0x40000000)
+		{
+			AT91C_BASE_PIOA->PIO_SODR = 0x00080000;
+			AT91C_BASE_PIOA->PIO_SODR = 0x80000000;
+		}
+		else
+		{
+			AT91C_BASE_PIOA->PIO_CODR = 0x00080000;
+			AT91C_BASE_PIOA->PIO_CODR = 0x80000000;
+		}
 	}
 
 	return 42;
